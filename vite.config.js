fa310ca4ab6db9e5c46060441c0e23806d29e6e6
Ziad Fathy <ziadfathy498@gmail.com>
2026-04-21 23:37:37 +0200

    aa

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Takamul/', // ضيف السطر ده بالظبط (تأكد من الحروف الكبيرة والصغيرة لاسم الريبو)
-})
