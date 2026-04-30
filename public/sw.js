6335a512ea390bc8fdf8ab5b89d5bc828755f2b9
Ziad Fathy <ziadfathy498@gmail.com>
2026-04-30 14:22:10 +0300

    add files

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'takamul-v1'
+const STATIC_ASSETS = [
+  '/Takamul/',
+  '/Takamul/index.html',
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  // للـ API calls (Supabase) - اتصل بالنت دايمًا
+  if (event.request.url.includes('supabase.co')) {
+    return
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  )
+})
