3cde5849322b806a8ecdda999909c7391846ab3f
Ziad Mohammed Fathy <114193419+ziadmohamed0@users.noreply.github.com>
2026-07-22 13:51:44 +0300

    fix: update base path to /tahakum-dashboard/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tahakom-v1-DashBoard/',
+  base: '/tahakum-dashboard/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
